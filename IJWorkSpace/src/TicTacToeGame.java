3ace5f17ec52f7681a4eff9e6553def951b3d510
AbhishekDesai56 <abhishekdesai56@gmail.com>
2021-05-22 11:36:55 +0530

    Initializing the AddressBookMain Class

---
+++ IJWorkSpace/src/TicTacToeGame.java
@@ -0,0 +1,19 @@
+public class TicTacToeGame {
+	public static void main(String args[]) {
+		TicTacToeGame ticTacTok = new  TicTacToeGame();
+		ticTacTok.Board();
+	}
+
+	public void Board() {
+		char[] board = new char[10];
+		board[0]=' ';
+		board[1]=' ';
+		board[2]=' ';
+		board[3]=' ';
+		board[4]=' ';
+		board[6]=' ';
+		board[7]=' ';
+		board[8]=' ';
+		board[9]=' ';
+	}
+}
